1c99196178eec07e705a9486f71bbe2ac0387211
Codersoumya26 <Panjasoumya@gmail.com>
2023-08-18 17:25:23 +0530

    Add User login page

--- routers/auth.py
+++ routers/auth.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import sys
+
 sys.path.append("..")
 
-
+templates = Jinja2Templates(directory="templates")
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
     responses={404: {"description": "Not found"}}
 )
+
+
+@router.get("/", response_class=HTMLResponse)
+async def get_authentication_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
